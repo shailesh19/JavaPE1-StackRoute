0c3d9f4c482d3e2978e2d12ee1ebf564b0d5a66e
shailesh <beingshailesh19@gmail.com>
2019-07-12 11:26:50 +0530

    questions added

--- src/main/java/com/stackroute/pe1/DisplayTotal.java
+++ src/main/java/com/stackroute/pe1/DisplayTotal.java
@@ -6,14 +6,25 @@ public class DisplayTotal
 {
     public static void main(String[] args)
     {
+        int sum=0;
         Scanner input = new Scanner(System.in);
-        int[] array = new int[4];
-        for(int i=1;i<=array.length;i++)
+        System.out.println(" Enter Number of integers ");
+        int number = input.nextInt();
+
+        int[] store = new int[number];
+        System.out.println(" Enter Numbers ");
+        for (int i=0; i<store.length; i++)
         {
-            array[i] = input.nextInt();
+            store[i] = input.nextInt();
+            sum = sum+store[i];
         }
-    }
+        System.out.println("Sum is: "+sum );
+        }
 }
 
 
 
+//        for(int i=1;i<=array.length;i++)
+//        {
+//            array[i] = input.nextInt();
+//        }
